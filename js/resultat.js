6e0a3fe80bbfe23953991e2957488d313a8651a5
Emile Schlick <emile.schlick@gmail.com>
2025-04-03 16:45:51 +0200

    Färdig js

---
+++ js/resultat.js
@@ -0,0 +1,13 @@
+const params = new URLSearchParams(window.location.search);
+const namn = params.get("namn");
+const poang = params.get("poang");
+const omgang = params.get("omgang");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const result = document.getElementById("resultText");
+    result.textContent = `Grattis, ${namn}! Du vann med ${poang} poäng på ${omgang} rundor!`;
+});
+
+function playAgain() {
+    window.location.href = `spel.html?namn=${encodeURIComponent(namn)}`;
+}
---
+++ js/script.js
@@ -0,0 +1,73 @@
+let namn = "";
+let omgang = 1;
+let poang = 0;
+let totalPoang = 0;
+let dice = 1;
+let harKastat = false;
+
+
+const dieFace = ['one', 'two', 'three', 'four', 'five', 'six'];
+
+function setNamn(){
+    const params = new URLSearchParams(window.location.search);
+    namn = params.get("namn");
+
+    if (namn) {
+        document.getElementById("namnBox").textContent = namn;
+    }
+}
+
+function setDice(){
+    dice = Math.floor(Math.random() * 6) + 1;
+    setDiceFace(dice);
+    countPoang(dice);
+    harKastat = true;
+}
+
+function setDiceFace(dice){
+    const allDice = document.querySelectorAll(".die");
+    allDice.forEach(die => die.classList.remove("show"));
+
+    
+    const dieToShow = document.querySelector(`.die.${dieFace[dice - 1]}`);
+    if (dieToShow) dieToShow.classList.add("show");
+}
+
+function countPoang(dice){
+    if (dice === 1) {
+        avslutaRunda();
+    } else {
+        poang += dice;
+        document.getElementById("rundanBox").innerText = poang;
+    }
+}
+
+function setPoang(){
+    if (!harKastat) return;
+
+    totalPoang += poang;
+    document.getElementById("totPoangBox").innerText = totalPoang;
+
+    avslutaRunda();
+
+    if (totalPoang >= 100) {
+        window.location.href = `Resultat.html?namn=${encodeURIComponent(namn)}&poang=${totalPoang}&omgang=${omgang}`;
+    }
+}
+
+function avslutaRunda() {
+    omgang++;
+    poang = 0;
+    harKastat = false;
+
+    document.getElementById("omgangBox").innerText = omgang;
+    document.getElementById("rundanBox").innerText = 0;
+}
+
+
+window.onload = (event) => {
+    setNamn();
+    document.getElementById("totPoangBox").innerText = totalPoang;
+    document.getElementById("omgangBox").innerText = omgang;
+    document.getElementById("rundanBox").innerText = 0;
+  };
